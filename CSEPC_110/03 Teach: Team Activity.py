578d31c55091ee70be2081caf1bf7b71930d4a01
nicholas richards <vrm.richards@gmail.com>
2021-09-30 11:38:48 -0600

    added an area for float

--- CSEPC_110/03 Teach: Team Activity.py
+++ CSEPC_110/03 Teach: Team Activity.py
@@ -25,13 +25,48 @@ print(f"The area of the rectangle is: ",area_of_rectangle,'\n')
 #circle stuff
 radius = int(input("What is the radius of the circle? "))
 area_of_circle = math.pi * (radius * radius)
-print(f"The area of the circle is:",area_of_circle)
+print(f"The area of the circle is:",area_of_circle,'\n')
 
 
 
 
+#===============================
+#float items
+#===============================
+print('==============================================')
+print('this is going to be the results as a decimal')
+print('==============================================','\n')
+
+#square stuff
+square_length = float(input("What is the length of a side of the square? (in cm) "))
+area_of_square = square_length ** square_length
+print(f"The area of the square is: ",area_of_square,"cm^2",'\n')
 
 
+#rectangle stuff
+rectangle_length = float(input("What is the length of a side of the rectangle? (in cm) "))
+rectangle_width = float(input("what is the width of the rectangle? (in cm) "))
+area_of_rectangle = rectangle_length * rectangle_width
+print(f"The area of the rectangle is: ",area_of_rectangle,"cm^2",'\n')
 
 
+#circle stuff
+radius = float(input("What is the radius of the circle? (in cm) "))
+area_of_circle = math.pi * (radius * radius)
+print(f"The area of the circle is:",area_of_circle,"cm^2",'\n')
 
+
+#===============================
+#float items
+#===============================
+
+
+print('==============================================')
+print('this is going to be the results as a decimal')
+print('==============================================','\n')
+
+
+#cube stuff
+square_length = float(input("What is the length of a side of the square? (in cm) "))
+area_of_square = square_length ** 3
+print(f"The area of the square is: ",area_of_square,"cm^2",'\n')
