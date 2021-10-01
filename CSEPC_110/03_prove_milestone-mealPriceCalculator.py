7d0d69f51eb118dc61fd4dcfdbbca736f461903a
Nr1cha <vrm.richards@gmail.com>
2021-09-30 19:29:34 -0600

    adding the file for the last assignment for the week

---
+++ CSEPC_110/03_prove_milestone-mealPriceCalculator.py
@@ -0,0 +1,26 @@
+childs_meal_float = float(input("what is the price of a child's meal? "))
+
+adults_meal_float = float(input("The price of an adult's meal"))
+
+number_of_children = int(input("The number of children "))
+
+number_of_adults = int(input("The number of adults "))
+
+sales_tax_rate = float(input("The sales tax rate "))
+
+#the math portion 
+
+# Determine the meal's subtotal (before adding sales tax)
+# by 
+# multiplying the number of children by the price of their meal,
+# and 
+# multiplying the number of adults by the price of their meal 
+# and 
+# adding those two values together.
+
+# OUTPUT
+print("mea'l subtotal:",number_of_children * childs_meal_float)
+print("mea'l subtotal:",number_of_adults * adults_meal_float)
+print("mea'l subtotal:", )
+print("mea'l subtotal:", )
+print("mea'l subtotal:", )
