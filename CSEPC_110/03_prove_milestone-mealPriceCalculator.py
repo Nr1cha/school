9df63100f0e755024e41e0acf6160f499b3a4f45
Nr1cha <vrm.richards@gmail.com>
2021-09-30 21:18:12 -0600

    adding stuff

--- CSEPC_110/03_prove_milestone-mealPriceCalculator.py
+++ CSEPC_110/03_prove_milestone-mealPriceCalculator.py
@@ -1,15 +1,3 @@
-childs_meal_float = float(input("what is the price of a child's meal? "))
-
-adults_meal_float = float(input("The price of an adult's meal"))
-
-number_of_children = int(input("The number of children "))
-
-number_of_adults = int(input("The number of adults "))
-
-sales_tax_rate = float(input("The sales tax rate "))
-
-#the math portion 
-
 # Determine the meal's subtotal (before adding sales tax)
 # by 
 # multiplying the number of children by the price of their meal,
@@ -17,10 +5,39 @@ sales_tax_rate = float(input("The sales tax rate "))
 # multiplying the number of adults by the price of their meal 
 # and 
 # adding those two values together.
+#------------
+#Compute and display the sales tax, 
+# by 
+# multiplying the subtotal by the sales tax rate divided by 100.
+#------------
+#Compute and display the total price of the meal 
+# by 
+# adding the subtotal and the sales tax.
+
+
+
+#INPUT PORTION
+childs_meal_float = float(input("what is the price of a child's meal? "))
+adults_meal_float = float(input("The price of an adult's meal? "))
+number_of_children = int(input("The number of children? "))
+number_of_adults = int(input("The number of adults? "))
+sales_tax_rate = float(input("what is the sales tax rate? "))
+
+
+
+#MATH LOGIC
+#-----------------
+subtotal = number_of_children * childs_meal_float + number_of_adults * adults_meal_float
+sales_tax = subtotal * sales_tax_rate / 100
+total_price_of_meal = subtotal + sales_tax
+#-----------------
 
 # OUTPUT
-print("mea'l subtotal:",number_of_children * childs_meal_float)
-print("mea'l subtotal:",number_of_adults * adults_meal_float)
-print("mea'l subtotal:", )
-print("mea'l subtotal:", )
-print("mea'l subtotal:", )
+print("childs meal subtotal$",number_of_children * childs_meal_float)
+print("adult meal subtotal$",number_of_adults * adults_meal_float)
+print("Subtotal: $",subtotal)
+print("sales tax$",sales_tax )
+print("meals total including sales tax$",total_price_of_meal)
+
+user_input_amount = float(input("what is the payment amount? "))
+print("change: $", user_input_amount - total_price_of_meal)
