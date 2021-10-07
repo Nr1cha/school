b8333a930e026123961654442a43f2642605caa7
nicholas richards <vrm.richards@gmail.com>
2021-10-07 01:06:30 -0600

    added math stuff and arranged the variables around

--- CSEPC_110/W04_teach_team_activity.py
+++ CSEPC_110/W04_teach_team_activity.py
@@ -1,3 +1,4 @@
+import math
 # Prompt the user for each of the variables described above.
 
 # Compute the value for the inner value c which is computed as:
@@ -6,8 +7,7 @@
 
 # Display the value c to three decimal places.
 
-# Compute and display the overall velocity. Display the value to three decimal places.
-
+# Compute and display the overall velocity. Display the value to three decimal places.`
 
 #WELCOME
 print("Welcome to the velocity calculator. Please enter the following: ","\n")
@@ -20,22 +20,27 @@ Density = float(input("Density of the fluid (in kg/m^3, 1.3 for air, 1000 for wa
 Cross_sectional_Area = float(input("Cross sectional area (in m^2): "))
 Drag_constant = float(input("Drag constant (0.5 for sphere, 1.1 for cylinder): "),"\n")
 
-
 #RESULT
 inner_value_of_c = float(input("The inner value of c is: "))
 velocity = float(input("The velocity after 10.0 seconds is: ", "m/s"))
+#
+#
+#
+#MATH STUFF
+m = Mass
 
+g = Gravity
 
+t = Time
 
+p = Density
 
+A = Cross_sectional_Area
 
+C = Drag_constant
 
+c = (1/2) * p * A * C
 
+exp = the number e (2.71828) to the given exponent. This can be computed in Python with the Math library function math.exp(value).
 
-
-
-
-
-
-
-
+sqrt = the square root of the given expression. This can be computed in Python with the Math library function math.sqrt(value).
