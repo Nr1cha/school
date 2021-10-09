df4fb7a9f0a7aa54b762134ac6b2b2747be26127
nicholas richards <vrm.richards@gmail.com>
2021-10-09 15:15:48 -0600

    adjusted seperation on output and changed capitalization

---
+++ CSEPC_110/W04/04_prove_milestone-mealPriceCalculator.py
@@ -0,0 +1,43 @@
+# Determine the meal's subtotal (before adding sales tax)
+# by 
+# multiplying the number of children by the price of their meal,
+# and 
+# multiplying the number of adults by the price of their meal 
+# and 
+# adding those two values together.
+#------------
+#Compute and display the sales tax, 
+# by 
+# multiplying the subtotal by the sales tax rate divided by 100.
+#------------
+#Compute and display the total price of the meal 
+# by 
+# adding the subtotal and the sales tax.
+
+
+
+#INPUT PORTION
+childs_meal_float = float(input("what is the price of a child's meal? "))
+adults_meal_float = float(input("The price of an adult's meal? "))
+number_of_children = int(input("The number of children? "))
+number_of_adults = int(input("The number of adults? "))
+sales_tax_rate = float(input("what is the sales tax rate? "))
+
+
+
+#MATH LOGIC
+#-----------------
+subtotal = number_of_children * childs_meal_float + number_of_adults * adults_meal_float
+sales_tax = subtotal * sales_tax_rate / 100
+total_price_of_meal = subtotal + sales_tax
+#-----------------
+
+# OUTPUT
+print("Childs meal subtotal $",number_of_children * childs_meal_float,sep="")
+print("Adult meal subtotal $",number_of_adults * adults_meal_float,sep="")
+print("Subtotal: $",subtotal,sep="")
+print("Sales tax $",round(sales_tax,2),sep="")
+print("Meals total including sales tax $",round(total_price_of_meal,2),sep="")
+
+user_input_amount = float(input("What is the payment amount? $"))
+print("change: $",round(user_input_amount - total_price_of_meal,2),sep="")
