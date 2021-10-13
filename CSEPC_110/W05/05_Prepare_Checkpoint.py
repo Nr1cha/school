0a92f1af87e17913d4a2d45dda5924f04ed5bfe2
Nr1cha <vrm.richards@gmail.com>
2021-10-12 21:40:31 -0600

    deleted files that dont need to be in the repo and finished prepare checkpoint file

--- CSEPC_110/W05/05_Prepare_Checkpoint.py
+++ CSEPC_110/W05/05_Prepare_Checkpoint.py
@@ -1,10 +1,48 @@
-first_name = input("What is your name? ")
-# second_number = input("What is the second number? ")
+# If the first number is greater than the second, 
+# print "The first number is greater". 
+# Otherwise, print "The first number is not greater".
 
-#LOGIC
-if first_name.lower() == "nick" \
-    or first_name.lower() == "brooke" \
-        or first_name.lower() == "nathaniel":
-    print("well hello",first_name,"ew lala")
+
+
+# If the two numbers are equal 
+# print "The numbers are equal". 
+# Otherwise, print "The numbers are not equal".
+
+
+
+# If the second number is greater than the first. 
+# print "The second number is greater". 
+# Otherwise, print "The second number is not greater".
+
+
+first_number = int(input("What is the first number? "))
+second_number = int(input("What is the second number? "))
+
+#FIRST PART
+if first_number > second_number:
+    print("The first number is greater",first_number,"\n")
 else: 
-    print ("I don't know you", first_name)
+    print ("The first number is not greater", first_number, "<", second_number,"\n")
+
+
+
+#SECOND PART
+if first_number == second_number:
+    print("The numbers are equal.", first_number, "==", second_number,"\n")
+else:
+    print("The numbers are not equal.",first_number,"!==", second_number,"\n")
+
+
+#THIRD PART
+if second_number > first_number:
+    print("The second number is greater.", second_number, ">", first_number,"\n")
+else:
+    print("The second number is not greater.",second_number,"<",first_number,"\n")
+
+
+#=============================
+favorite_color = input("What is your favorite color? ")
+if favorite_color.lower() == "blue":
+    print("That's awesome! My favorite color is also", favorite_color)
+else:
+    print(favorite_color, "is not my favorite color")
