690248a2b00a975d7821f13050407923a4ace3f1
Nr1cha <vrm.richards@gmail.com>
2021-10-21 22:43:53 -0600

    adding files and testing concepts

--- CSEPC_110/testStuff/scratch_paper.py
+++ CSEPC_110/testStuff/scratch_paper.py
@@ -1,18 +1,34 @@
-import ascii_magic
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
-nick = None
-try:
-    nick = ascii_magic.from_url("https://universe.byu.edu/wp-content/uploads/2015/11/Modern-Sailor-Cougar1-300x279.jpg",
-        columns=100,
-        width_ratio=2,
-    # mode=ascii_magic.Modes.HTML
-    )
-except OSError as e:
-    print(f"could not load the image, server said: {e.code}")
-# ascii_magic.to_terminal(nick)
-print(nick)
+# user_age = int(input("what is your age? "))
+# can_drive = False
 
 
-#output
+# if user_age >= 16:
+#     can_drive = True
+#     print("nice, you can drive")
+# elif user_age == 13:
+#     can_drive = True
+#     print("you slick cheater. good on ya")
+# else:
+#     can_drive = False 
+#     print("you need to grow up more.")
+
+
+
+
+
+# Python3 code to demonstrate working of
+# Converting String to binary
+# Using join() + ord() + format()
+  
+# initializing string 
+test_str = input("what is the text you want to convert to binary? ")
+  
+# printing original string 
+print("The original string is : " + str(test_str))
+  
+# using join() + ord() + format()
+# Converting String to binary
+res = ''.join(format(ord(i), '08b') for i in test_str)
+  
+# printing result 
+print("The string after binary conversion : " + str(res))
