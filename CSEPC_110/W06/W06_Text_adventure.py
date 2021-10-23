febed07e2d7cb31ffd71e2910dc8cd8fddc9d252
nicholas richards <vrm.richards@gmail.com>
2021-10-23 16:38:20 -0600

    redacted ascii_magic and cleaned up spacing. added design tree in code

--- CSEPC_110/W06/W06_Text_adventure.py
+++ CSEPC_110/W06/W06_Text_adventure.py
@@ -1,56 +1,73 @@
-from os.path import dirname, join
-current_dir = dirname(__file__)
-import ascii_magic
+# 1 YES
+    ##FIGHTER##
+    # 1.2 SWORD
+        # FIGHT 
+            # DEATH
+        # FLEE
+            # MOVE TO THE MOUNTAIN
+                #1.3 MOUNTAIN
+                #
+                # RUN 
+                    #DEATH
+                # SNEAK
+                    #WIN
 
-death = ascii_magic.from_image_file(join(current_dir, "./skull.jpeg"),
-                                    columns=60,
-                                    width_ratio=2,
-                                    )
+    ##WIZARD##
+    # 1.2 WAND
+        # ATTACK
+            # DEATH
+        # STUN
+            # MOVE TO THE MOUNTAIN
+                #1.3 MOUNTAIN
+                # 
+                # RUN
+                    #DEATH
+                # SNEAK
+                    #WIN
+# 2 NO
 
+#BEGINNING OF STORY
 answer = input("would you like to playa game? YES or NO ")
 
-if answer.lower().strip() == "yes":
+if answer.lower() == "yes":
 
-    weapon = input("You awake and find yourself in a dark and thick forested woods on a foggy night, before going on an adventure you must choose a weapon for protection. Which do you choose, SWORD or WAND? ").lower().strip()
+    weapon = input("You awake and find yourself in a dark and thick forested woods on a foggy night, before going on an adventure you must choose a weapon for protection. Which do you choose, SWORD or WAND? ").lower()
     if weapon == "sword" \
             or "wand":
-        print("")
-        answer = input(
-            "You encounter a deadly troll, you have two options, you can either risk your life and try to defeat the troll or flee. FIGHT or FLEE? ")
+        answer = input("You encounter a deadly troll, you have two options, you can either risk your life and try to defeat the troll or flee. FIGHT or FLEE? ")
         if answer.lower() == "fight":
             print(f"The troll swings his heavy club over your head, barely missing your neck. You can feel the goosebumps on your skin rise. You take your {weapon}, rising it directly over your head...")
             if weapon.lower() == "sword":
                 print("Your attack does no damage to the massive troll. The club comes at you again, this time landing directly upon your head.")
-                print(f"You have died. Try again.\n{death}")
-
+                print(f"You have died. Try again.")
+            #WIZARD PART
             else:
                 spell = input(
                     "What spell would you like to cast, STUN or ATTACK?")
                 if spell.lower() == "stun":
-                    print(
-                        "The troll freezes in his tracks. You consider this a win and head home.")
+                    print("The troll freezes in his tracks. You consider this a win and head home.")
                 else:
-                    print(
-                        "Your attack does no damage to the massive troll. The club comes at you again, this time landing directly upon your head.")
+                    print("Your attack does no damage to the massive troll. The club comes at you again, this time landing directly upon your head.")
                     print("You have died. Try again.")
         else:
+            # MOUNTAIN SECTION
             print("")
             print("you run quickly into the forest, outrunning the big slow troll.")
             print("You then come across a large cave in the side of a mountain. \nOutside the cave you see a scratched-up wooden sign, on it you can bearly make out the words 'please knock, all those who enter here'.")
             print("Thinking how odd of a sign that would be, you decide 'why not, seems nice', \nIn it you find a mess of Gold scattered about and a low breeze of air that seems to get stronger as you traverse deeper into the cave. ")
             print("As you round the corner, you find a sleeping dragon")
             print("")
+            #RUN AND DEATH PART
             answer = input("After picking up some gold, you decide it's time to leave, upon seeing the dragon move as if close to waking up, you think to yourself, \n'should i run or sneak out'? RUN or SNEAK ")
             if answer.lower() == "run":
                 print("")
-                print(
-                    "The dragon wakes up and sees you running. Upon spewing fire all around you, you turn into a pile of ash.")
+                print("The dragon wakes up and sees you running. Upon spewing fire all around you, you turn into a pile of ash.")
                 print("you have died, better luck on the next respawn")
             else:
+            #SNEAK ESCAPE PART
                 print("")
                 print("you make it out alive without disturbing the dragon. in a low valley you find a town to exchange your gold for riches and then the story ends ")
                 print("keep watch for more DLC (which is free BTW, none of that microtransactiony fluff) to see new features and stories.")
-
     else:
         print("well that sucks")
 else:
