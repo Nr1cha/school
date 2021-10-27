514ad5d2e5e26661799afa7605a682be65843da8
Nr1cha <vrm.richards@gmail.com>
2021-10-26 21:05:03 -0600

    added pixel changes to picture and added pictures

--- CSEPC_110/W07/07_prove_assignment_milestone.py
+++ CSEPC_110/W07/07_prove_assignment_milestone.py
@@ -1,2 +1,20 @@
-from PIL import ImageColor
-print("pillow is working")
+from PIL import Image
+print("the library is working correctly")
+image_original = Image.open("beach.jpg")
+print(image_original.size)
+print(image_original.format)
+# image_original.show()
+#load pixels
+pixels_original = image_original.load()
+
+
+print(pixels_original[222, 100])
+pixels_original[222,100] = (255, 0, 255)
+pixels_original[223,100] = (255, 0, 255)
+pixels_original[224,100] = (255, 0, 255)
+pixels_original[225,100] = (255, 0, 255)
+pixels_original[226,100] = (255, 0, 255)
+image_original.show()
+
+
+
