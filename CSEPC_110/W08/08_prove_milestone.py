7a0eec196e697678258dd176b1e69569c735cc3c
Nr1cha <vrm.richards@gmail.com>
2021-11-06 13:40:13 -0600

    adding pictures to w08 directory

--- CSEPC_110/W08/08_prove_milestone.py
+++ CSEPC_110/W08/08_prove_milestone.py
@@ -2,9 +2,33 @@ import os
 os.system("clear")
 from PIL import Image
 
+#FORMATTING
+u = "\033[4m"
+b = "\033[1m"
+i = "\033[3m"
+Black = "\u001b[30m"
+Red = "\u001b[31m"
+Green = "\u001b[32m"
+Yellow = "\u001b[33m"
+Blue = "\u001b[34m"
+Magenta = "\u001b[35m"
+Cyan = "\u001b[36m"
+White = "\u001b[37m"
+Reset = "\u001b[0m"
+
+#ARRAY STUFF
+foreground_images = ["harvester","cactus","cat_small","cat","hiker","penguin","boat","spaceshuttle",]
+background_images = ["earth","snowscape","desert","field","forest","sunset",]
+
+#PRINT USER INPUT
+print(f"{Green}{foreground_images}{Reset}\n")
+user_foreground = input("pick a foreground image: ")
+print(f"{Blue}{background_images}{Reset}\n")
+user_background = input("pick a background image: ")
+
 #IMAGES
-BG_image = Image.open("earth.jpg")
-FG_image = Image.open("spaceshuttle.jpg")
+BG_image = Image.open(f"{user_background}.jpg")
+FG_image = Image.open(f"{user_foreground}.jpg")
 image_combined = Image.new("RGB", FG_image.size)
 FG_pixels = FG_image.load()
 BG_pixels = BG_image.load()
