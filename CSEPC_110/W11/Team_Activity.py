f5eb263a874141363ce678dee5339929c31eba08
nicholas richards <vrm.richards@gmail.com>
2021-11-25 13:25:34 -0700

    did stretch challenge

--- CSEPC_110/W11/Team_Activity.py
+++ CSEPC_110/W11/Team_Activity.py
@@ -1,6 +1,13 @@
 with open("hr_system.txt") as HR_list:
     for line in HR_list:
         clean_line = line.split()
-        names = clean_line[0]
+        name = clean_line[0]
+        ID_Number = clean_line[1]
         job_title = clean_line[2]
-        print(f"Name: {names} \nJob Title: {job_title}\n")
+        salary = float(clean_line[3])
+        divided_salary = salary / 24
+        if job_title == "Engineer" and divided_salary:
+            print(f"Name: {name} ID Number: ({ID_Number}), Job Title: {job_title} - ${divided_salary + 1000:.2f}\n")
+        if job_title != "Engineer":
+            print(f"Name: {name} ID Number: ({ID_Number}), Job Title: {job_title} - ${divided_salary:.2f}\n")
+        # print(f"Name: {name} ID Number: ({ID_Number}), Job Title: {job_title} - ${divided_salary:.2f}\n")
