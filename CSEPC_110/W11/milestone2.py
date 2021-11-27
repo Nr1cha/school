b6d29624ef6c04faf1fe20c15f22397008f45ea9
Nr1cha <vrm.richards@gmail.com>
2021-11-27 12:02:01 -0700

    added more logic to milestone

--- CSEPC_110/W11/milestone2.py
+++ CSEPC_110/W11/milestone2.py
@@ -1,24 +1,5 @@
-# list_expectency = []
-# with open("life-expectancy.csv") as life_expetancy:
-#     for line in life_expetancy:
-#         if line.startswith("Entity"): continue;  #this will skip the first line
-#         #line format
-#         clean_line = line.split(",")
-#         striped_line = line.strip()
-
-#         #variables
-#         entity = striped_line[0]
-#         code = striped_line[1]
-#         year = int(clean_line[2])
-#         expectency = float(clean_line[3])
-#         # sorted = year.sort(reverse=True)
-#         # print(expectency)
-#         list_expectency.append(expectency)
-#     list_expectency.sort(reverse = False)
-
-#     print(list_expectency)
-
-
+import os 
+os.system("clear")
 
 user_choice = None
 max_life_expectancy = None
@@ -27,25 +8,48 @@ max_life_expectancy_entity = None
 min_life_expectancy = None
 min_life_expectancy_year = None
 min_life_expectancy_entity = None
-country = None
+
+
+
+
+min_expectancy = None
+min_country1 = None
+max_expectancy = None
+max_country2 = None
+
+
+
+
+array_expectancy = []
 
 with open("life-expectancy.csv") as life_expectency_file:
-
-    # user_choice = int(input(f"Enter the year of interest: "))
-    # # print(f"The overall max life expectancy is:")
-    # print(f"The overall min life expectancy is:")
-    # print(f"For the year {}:")
-    # print(f"The average life expectancy across all countries was {}")
-
-
+    user_choice = input("Enter the year of interest:")
     for line in life_expectency_file:
         if line.startswith("Entity"): continue;
         clean_line = line.split(",")
-        # striped_line = line.strip()
+
         entity = clean_line[0]
         code = clean_line[1]
         year = clean_line[2]
         expectency = clean_line[3]
+
+        if user_choice == year:
+            array_expectancy.append(float(expectency))
+
+
+
+
+
+        if user_choice == year and (min_expectancy == None or expectency < min_expectancy):
+            min_expectancy = expectency
+            min_country1 = entity
+
+        # if user_choice == year and expectency > max_expectancy:
+            
+        #     max_expectancy = expectency
+        #     max_country2 = entity
+
+
 
 
         if min_life_expectancy == None or expectency < min_life_expectancy:
@@ -53,11 +57,21 @@ with open("life-expectancy.csv") as life_expectency_file:
             min_life_expectancy_year = year
             min_country = entity
         
-
         if max_life_expectancy == None or expectency > max_life_expectancy:
             max_life_expectancy = expectency 
             max_life_expectancy_year = year
             max_country = entity
 
+    life_average_age = sum(array_expectancy) / len(array_expectancy)
+
     print(f"The overall min life expectancy is: {min_life_expectancy} from {min_country} in {min_life_expectancy_year}")
     print(f"The overall max life expectancy is: {max_life_expectancy} from {max_country} in {max_life_expectancy_year}")
+    print()
+    print(f"For the year {user_choice}:")
+    print(f"The average life expectancy across all countries was {life_average_age:.2f}")
+
+
+
+
+    print(f"The max life expectancy was in {max_country2} with {max_expectancy}")
+    print(f"The min life expectancy was in {min_country1} with {min_expectancy}")
