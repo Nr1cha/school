3d6e9ab892d76ebee746cd1f9b911657423a5f76
Nr1cha <vrm.richards@gmail.com>
2021-11-27 16:52:29 -0700

    adjusted formatting

--- CSEPC_110/W11/milestone2.py
+++ CSEPC_110/W11/milestone2.py
@@ -1,6 +1,20 @@
 import os 
 os.system("clear")
 
+u = "\033[4m"
+b = "\033[1m"
+i = "\033[3m"
+Black = "\u001b[30m"
+Red = "\u001b[31m"
+Green = "\u001b[32m"
+Yellow = "\u001b[33m"
+Blue = "\u001b[34m"
+Magenta = "\u001b[35m"
+Cyan = "\u001b[36m"
+White = "\u001b[37m"
+Reset = "\u001b[0m"
+
+
 user_choice = None
 max_life_expectancy = None
 max_life_expectancy_year = None
@@ -8,49 +22,35 @@ max_life_expectancy_entity = None
 min_life_expectancy = None
 min_life_expectancy_year = None
 min_life_expectancy_entity = None
-
-
-
-
+#2nd data
 min_expectancy = None
 min_country1 = None
 max_expectancy = None
 max_country2 = None
-
-
-
-
+#array
 array_expectancy = []
 
 with open("life-expectancy.csv") as life_expectency_file:
-    user_choice = input("Enter the year of interest:")
+    user_choice = input("Enter the year of interest: ")
     for line in life_expectency_file:
         if line.startswith("Entity"): continue;
         clean_line = line.split(",")
-
+#positions
         entity = clean_line[0]
         code = clean_line[1]
         year = clean_line[2]
-        expectency = clean_line[3]
-
+        expectency = float(clean_line[3])
+#if statements
         if user_choice == year:
-            array_expectancy.append(float(expectency))
-
-
-
-
-
+            array_expectancy.append(float(expectency)) # adding expectency to array outside of loop
+# LOGIC STUFF
         if user_choice == year and (min_expectancy == None or expectency < min_expectancy):
             min_expectancy = expectency
             min_country1 = entity
 
-        # if user_choice == year and expectency > max_expectancy:
-            
-        #     max_expectancy = expectency
-        #     max_country2 = entity
-
-
-
+        if user_choice == year and (max_expectancy == None or expectency > max_expectancy):
+            max_expectancy = expectency
+            max_country2 = entity
 
         if min_life_expectancy == None or expectency < min_life_expectancy:
             min_life_expectancy = expectency
@@ -66,12 +66,8 @@ with open("life-expectancy.csv") as life_expectency_file:
 
     print(f"The overall min life expectancy is: {min_life_expectancy} from {min_country} in {min_life_expectancy_year}")
     print(f"The overall max life expectancy is: {max_life_expectancy} from {max_country} in {max_life_expectancy_year}")
-    print()
-    print(f"For the year {user_choice}:")
-    print(f"The average life expectancy across all countries was {life_average_age:.2f}")
-
-
-
-
-    print(f"The max life expectancy was in {max_country2} with {max_expectancy}")
-    print(f"The min life expectancy was in {min_country1} with {min_expectancy}")
+    print("")
+    print(f"For the year {u}{user_choice}{Reset}:")
+    print(f"The average life expectancy across all countries was {Green}{life_average_age:.2f}{Reset}")
+    print(f"The max life expectancy was in {u}{max_country2}{Reset} with {Green}{max_expectancy:.2f}{Reset}")
+    print(f"The min life expectancy was in {u}{min_country1}{Reset} with {Green}{min_expectancy:.2f}{Reset}")
