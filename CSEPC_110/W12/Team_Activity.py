f8716992e886b5ee53a7891557df260ddc9e82d3
Nr1cha <vrm.richards@gmail.com>
2021-12-02 22:17:22 -0700

    fixing indentation

--- CSEPC_110/W12/Team_Activity.py
+++ CSEPC_110/W12/Team_Activity.py
@@ -1,11 +1,12 @@
-#book
-#chapter
-#volume
-import os 
+# book
+# chapter
+# volume
+import os
 os.system('clear')
 # from types import MethodDescriptorType
 
-chosen_volume = input("Which volume of scripture would you like to learn about? ")
+chosen_volume = input(
+    "Which volume of scripture would you like to learn about? ")
 
 # books = []
 # chapters = []
@@ -17,33 +18,33 @@ book_volume = None
 
 with open("books_and_chapters.txt") as scriptures:
 
-        for lines in scriptures:
-                clean_line = lines.split(":")
-                book = clean_line[0].strip() #!# bible, book of mormon etc. 
-                chapter = int(clean_line[1]) #!# chapter number
-                volume = clean_line[2].strip() #!# nephi, alma, james, matthew etc. 
+    for lines in scriptures:
+        clean_line = lines.split(":")
+        book = clean_line[0].strip()  # !# bible, book of mormon etc.
+        chapter = int(clean_line[1])  # !# chapter number
+        volume = clean_line[2].strip()  # !# nephi, alma, james, matthew etc.
 
-        #adding to my arrays
-                # books.append(book)
-                # chapters.append(chapter)
-                # volumes.append(volume)
-# new portion
+    # adding to my arrays
+        # books.append(book)
+        # chapters.append(chapter)
+        # volumes.append(volume)
+        # new portion
+
         if volume.lower() == chosen_volume.lower():
-                print(f"Scripture: {volume}, Book: {book}, Chapters: {chapter}")
+            print(f"Scripture: {volume}, Book: {book}, Chapters: {chapter}")
 
-                # if chapter > most_chapters:
-                #         most_chapters = chapter
-                #         book_volume = volume
+            # if chapter > most_chapters:
+            #         most_chapters = chapter
+            #         book_volume = volume
 
+        # find the book with the most chapters
+            if most_chapters == None or chapter > most_chapters:
+                most_chapters = chapter
+                book_with_most_chapters = book
+                book_volume = volume
 
-
-        #find the book with the most chapters
-                if most_chapters == None or chapter > most_chapters:
-                        most_chapters = chapter
-                        book_with_most_chapters = book
-                        book_volume = volume
-        
 # print(volume)
 # print(f"Scripture: {book_volume}, Book: {book_with_most_chapters}, Chapters: {most_chapters}")
-print(f"The book with the most chapters in the {chosen_volume} is: {most_chapters}")
+print(
+    f"The book with the most chapters in the {chosen_volume} is: {book_with_most_chapters}")
 print(f"It has {most_chapters} chapters.")
