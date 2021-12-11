41378c995ee44dea339f12ad2a8b698292fd2dd2
Nr1cha <vrm.richards@gmail.com>
2021-12-11 12:35:50 -0700

    created statements for user input change options

--- CSEPC_110/W13/Prove.py
+++ CSEPC_110/W13/Prove.py
@@ -10,10 +10,8 @@ os.system("clear")
 #01 
 #Write a function to calculate and return the wind chill based on a given temperature and wind speed.
 def Calculate_wind_chill(temperature, wind_speed):
-    print("this thing")
-    print(f"temperature:{temperature}, windspeed:{wind_speed}")
-
     wind_chill = 35.74 + 0.6215 * temperature - 35.75 * (wind_speed**0.16) + 0.4275 * temperature * (wind_speed**0.16)
+    
     return wind_chill
 
 input_temperature = 8
@@ -25,31 +23,26 @@ print(f"At temperature {input_temperature}F, and windspeed {input_windspeed} mph
 
 
 
-
-
-
 def C_to_F_Conversion(Celsius):
-    
     Fahrenheit = (Celsius * (9 / 5)) + 32
-    print()
-
-
-
 
     return Fahrenheit
-celcius_input = 79
-Temp_output = C_to_F_Conversion(celcius_input)
-print(f"{celcius_input} degrees celcius is {Temp_output:.2f} degrees Fahrenheit")
 
 
 
 
+# celcius_input = 79
+# Temp_output = C_to_F_Conversion(celcius_input)
+# print(f"{celcius_input} degrees celcius is {Temp_output:.2f} degrees Fahrenheit")
 
 
 
-# #PRINT QUESTIONS
-# user_temp = float(input("What is the temperature? "))
-# temp_type = input("Fahrenheit or Celsius (F/C)? ")
+#PRINT QUESTIONS
+user_temp = float(input("What is the temperature? "))
+temp_type = input("Fahrenheit or Celsius (F/C)? ")
 
-# if temp_type.lower() == "f":
-#     print() #INPUT Fahrenheit DATA
+if temp_type.lower() == "f":
+    print(f"the temperature is: {user_temp} Degrees Fahrenheit.") #INPUT Fahrenheit DATA
+elif temp_type.lower() == "c":
+    Temperature_Fah = C_to_F_Conversion(user_temp)
+    print(f"the new temperature is: {Temperature_Fah} Degrees Fahrenheit.")
