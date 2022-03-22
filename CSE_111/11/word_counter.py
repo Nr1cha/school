e11b97b0611ac526b9c2676ccb06fae57d540003
nicholas richards <vrm.richards@gmail.com>
2022-03-22 10:37:39 -0600

    adding word counter project for last week final

---
+++ CSE_111/11/word_counter.py
@@ -0,0 +1,13 @@
+# https://pythonexamples.org/python-count-occurrences-of-word-in-text-file/
+
+
+#get file object reference to the file
+file = open("/Users/nick/Documents/GitHub/school/CSE_111/11/words.txt", "r")
+
+#read content of file to string
+data = file.read().lower()
+
+#get number of occurrences of the substring in the string
+occurrences = data.count("what")
+
+print('Number of occurrences of the word :', occurrences)
