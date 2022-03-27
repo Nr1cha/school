7a0cd6ab70bcc21ff95ae209e022a04be48c77c3
Nick Richards <vrm.richards@gmail.com>
2022-03-26 22:15:02 -0600

    added to file for testing

--- CSE_111/12/test_word_counter.py
+++ CSE_111/12/test_word_counter.py
@@ -2,11 +2,8 @@ from word_counter import read_ext_file, user_input
 from pytest import approx
 import pytest
 
-
-def read_ext_file():
+def test_read_ext_file():
 
     variable = read_ext_file()
 
     assert isinstance(variable, "words2.txt")
-
-    check_element
