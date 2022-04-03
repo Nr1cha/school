fbac6ac01373ba27bf50fdf1270d0f49e5871d0f
Nr1cha <vrm.richards@gmail.com>
2022-04-02 20:55:55 -0600

    added better filename and better file input

--- CSE_111/13/testFile.py
+++ CSE_111/13/testFile.py
@@ -12,12 +12,11 @@ i_after = fruit[fruit_position + 1]
 
 #Use Known Entites In The List To Find The Index Of An Unknown Object
 Index_Number_For_Bannas = fruit.index('apples')
-
+# indexes = []
 for i in range(len(fruit)):
     if fruit[i] == chosen_word:
         # print(type(i_before))
         print(f"the word before: {i_before}, chosen word {fruit[i]}, at possition:{i}, the word after: {i_after}")
-
 
 #get items before and after the selected word. context
 word = stuff.index(user_word)
@@ -26,10 +25,20 @@ a = stuff[word - 1]
 b = stuff[word + 1]
 
 # print(type(a))
-print(a, user_word, b)
+# print(a, user_word, b)
 
 
 
 ext_file = open("words13.txt", "r")
-print(ext_file)
-print(type(ext_file))
+file_string = ext_file.read()
+new_file_list = list(file_string)
+# print(type(file_string))
+# print(file_string)
+
+
+bad_characters = [1,2,3,4,5,6,7,8,9,0,'.',',','"',"'",'{','}','?','(',')','\\','*',';','1','2','3','4','5','6','7','8','9','0']
+for i in bad_characters :
+    file_string = ''.join((filter(lambda i: i not in bad_characters, new_file_list)))
+# print(str(file_string))
+# print(type(file_string))
+
--- CSE_111/13/word_counter.py
+++ CSE_111/13/word_counter.py
@@ -4,45 +4,53 @@ import os
 os.system('clear')
 
 #GLOBAL VARIABLES
-userFile = input("please type in the name of the file. ")
-user_word = input("what word do you want to search? ").lower().capitalize()
-#todo add a global list of the contents of the file
+userFile = input("please type in the name of the file: ") + ".txt"
+print(userFile)
+user_word = input("what word do you want to search? ").lower()
+gindex_list = []
 
 
 def main(user_file_name):#the main function that holds the stuff i want the file to do.
 
     file_contents = read_ext_file(user_file_name)#this is a string
-    word_output = user_input(user_word, file_contents)#this is an integer
-    clean_file(file_contents)
-    print(f" '{user_word}' has {word_output} occurances in your file.")
+    file_contents2 = all_text_to_lower(file_contents)
+    finished_file = clean_file(file_contents2)
+    # print(finished_file, type(finished_file))
+    word_output = get_word_occurances(user_word, finished_file)#this is an integer
+    
+    print(f" '{user_word}' has {word_output[0]} occurances at position(s) {word_output[1]}")
+    return
 
-def clean_file(file_variable):
-    bad_characters = [1,2,3,4,5,6,7,8,9,0,'.',',','"',"'",'',]#list of characters i don't want in my file
-    for i in bad_characters:
-        clean_line = file_variable.replace(i, '')#replacing them here
+def clean_file(file_variable):#*done
+    bad_characters = [1,2,3,4,5,6,7,8,9,0,'.',',','"','{','}','?','(',')','//\\//\\','*',';','1','2','3','4','5','6','7','8','9','0']    #list of characters i don't want in my file
+    # for i in bad_characters:
+    cleaned_file = ''.join((filter(lambda i: i not in bad_characters, file_variable)))#clean stuff
+    return cleaned_file
 
-def all_text_to_lower(clean_file):#this function will convert all text in the file to lowercase
-    print()
-    return clean_file
+def all_text_to_lower(clean_file):#* this function will convert all text in the file to lowercase
 
-def read_ext_file(filename): #read the text of an external file. 
+    a =   clean_file.lower()#takes file and converts to lowercase
+    return a
+
+def read_ext_file(filename): #*read the text of an external file. 
 
     try:#surround it with error checking
-        #assign file to a variable
-        ext_file = open(filename, "r")
-        #read content of file to string
-        file_contents = ext_file.read()
+
+        ext_file = open(filename, "r") #assign file to a variable
+        file_contents = ext_file.read() #read content of file to string
         return file_contents
         
     except FileNotFoundError as fileIssue:#if the item is incorrect, this will run
         print(f"incorrect filename. please type in a filename that exists. {fileIssue}")
         sys.exit(1)#this is to exit once it runs and stops it from running again
     
-def user_input(user_word, file_contents):#get number of occurrences of the substring in the string
-
-    word_occurrences = file_contents.count(user_word)
-
-    return word_occurrences
+def get_word_occurances(user_word, file_contents):#*get number of occurrences of the substring in the string
+    file_contents_list = list(file_contents.split(" "))
+    word_occurrences = file_contents_list.count(user_word)
+    for i in range(len(file_contents_list)):
+        if file_contents_list[i] == user_word:
+            gindex_list.append(i)
+    return word_occurrences, gindex_list
 
 if __name__ == "__main__":
 
