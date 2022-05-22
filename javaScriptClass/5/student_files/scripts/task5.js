576d42e9895ee06b10f7d295092ac06a01f06a04
Nick Richards <vrm.richards@gmail.com>
2022-05-21 19:28:17 -0600

    adding syntax for fetchreq

--- javaScriptClass/5/student_files/scripts/task5.js
+++ javaScriptClass/5/student_files/scripts/task5.js
@@ -69,7 +69,7 @@ document.getElementById("message2").innerHTML = anotherMessage1;
 let templeList = [];
 
 // Step 2: Declare a function named output that accepts a list of temples as an array argument and does the following for each temple:
-function output(listOfTemples) {
+function output1(listOfTemples) {
     for (temple of listOfTemples) {
 
         let templeId = document.getElementById("temples");
@@ -97,13 +97,25 @@ function output(listOfTemples) {
 // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
 // - Appends the <article> element to the HTML element with an ID of temples
 
-templeList.push({ templeName: "testTemple", location: "testLocation", dedicated: "testDedication", imageUrl: "www.google.com" })
-output(templeList);
+templeList.push({ templeName: "testTemple", location: "testLocation", dedicated: "testDedication", imageUrl: "https://ksltv.com/wp-content/uploads/2022/03/SMITHFIELD-EPHRAIM-RENDERS.jpg" })
+output1(templeList);
 
 // Step 3: Create another function called getTemples. Make it an async function.
+function getTemples(){
+
+}
 // Step 4: In the function, using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'. Create a variable to hold the response from your fetch. You should have the program wait on this line until it finishes.
 // Step 5: Convert your fetch response into a Javascript object ( hint: .json() ). Store this in the templeList variable you declared earlier (Step 1). Make sure the the execution of the code waits here as well until it finishes.
 // Step 6: Finally, call the output function and pass it the list of temples. Execute your getTemples function to make sure it works correctly.
+
+async function getTempleData() {
+    let remoteData = await fetch("https://byui-cse.github.io/cse121b-course/week05/temples.json");
+    remoteData = await remoteData.json();
+    // output(remoteData);
+    console.log(remoteData)
+}
+getTempleData();
+
 
 // Step 7: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
 
