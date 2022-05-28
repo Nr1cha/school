9a311f624dc88d719ba94b27a1bdf9ddc92d49a3
Nick Richards <vrm.richards@gmail.com>
2022-05-28 17:19:55 -0600

    adding final changes

--- javaScriptClass/6/scripts/task6.js
+++ javaScriptClass/6/scripts/task6.js
@@ -1,74 +1,68 @@
-
 let dateNow = new Date();
 let currentDay = dateNow.getDay();
 
-let message = "hang in there"
-
-if (currentDay == 1, 2, 3, 4, 5) {
-    message = "Hang in there!";
-}else {
-    message = "Woohoo! it is the weekend!";
-}
-
-/* SWITCH, CASE, BREAK 
-day*/
-let anotherMessage1 = "";
-
+/* SWITCH, CASE, BREAK */
+let day;
 switch (currentDay) {
     case 0:
-        anotherMessage1 = "Sunday";
+        day = "Sunday";
         break;
     case 1:
-        anotherMessage1 = "Monday";
+        day = "Monday";
         break;
     case 2:
-        anotherMessage1 = "Tuesday";
+        day = "Tuesday";
         break;
     case 3:
-        anotherMessage1 = "Wednesday";
+        day = "Wednesday";
         break;
     case 4:
-        anotherMessage1 = "Thursday";
+        day = "Thursday";
         break;
     case 5:
-        anotherMessage1 = "Friday";
+        day = "Friday";
         break;
     case 6:
-        anotherMessage1 = "Saturday";
+        day = "Saturday";
         break;
 }
 
-document.getElementById("message1").innerHTML = message;
-document.getElementById("message2").innerHTML = anotherMessage1;
+if ([1, 2, 3, 4, 5].includes(currentDay)) {
+    message = "You're almost there!";
+}
+else {
+    message = "Finally! it is the weekend!";
+}
+document.getElementById("day").innerHTML = day;
+document.getElementById("message").innerHTML = message;
 
-
-/* FETCH */
+// global list
 let filmList = [];
 
 
 function output(listOfNames) {
     for (film of listOfNames) {
-        // let nL = document.createElement("br");
         let templeId = document.getElementById("movies");
         let articleElement = document.createElement("article");
 
-        let h3Tag = document.createElement("h3")
+        let h3Tag = document.createElement("h3");
         h3Tag.innerText = film.title;
 
-        let movieDate = document.createElement("h3")
+        let movieDate = document.createElement("h3");
         movieDate = "Date Released: " + film.release_date;
 
         let h4Tag = document.createElement("h4");
-        h4Tag.innerText = "Producer: " + film.producer
+        h4Tag.innerText = "Producer: " + film.producer;
 
         let h4_2Tag = document.createElement("h4");
-        h4_2Tag = "Director: " + film.director
+        h4_2Tag = "Director: " + film.director;
 
         articleElement.append(h3Tag, movieDate, h4Tag, h4_2Tag);
         templeId.append(articleElement);
 
     }
 }
+
 
 async function getMovies() {
     let remoteData = await fetch("https://swapi.dev/api/films");
@@ -84,41 +78,40 @@ function reset() {
 }
 
 // sort function
-function sortBy(){
+function sortBy() {
     reset();
     let sort = document.getElementById("sortBy").value;
-    if (sort ==  "filmsAscending") {
+    if (sort == "filmsAscending") {
         filmList.sort(sortAscending);
     }
-    if (sort == "filmsDescending"){
+    if (sort == "filmsDescending") {
         filmList.sort(sortDescending);
-        // console.log(filmList)
     }
     output(filmList);
 }
 
 // sort descending
-function sortDescending (a,b){
-    if (a.title < b.title){
+function sortDescending(a, b) {
+    if (a.title < b.title) {
         return 1;
     }
-    if (a.title > b.title){
+    if (a.title > b.title) {
         return -1;
     }
     return 0;
 }
 
 // sort ascending
-function sortAscending (a,b){
-    if (a.title < b.title){
+function sortAscending(a, b) {
+    if (a.title < b.title) {
         return -1;
     }
-    if (a.title > b.title){
+    if (a.title > b.title) {
         return 1;
     }
     return 0;
 }
 
 //  event listener that calls the sortBy function
-document.getElementById("sortBy").addEventListener("change",sortBy);
+document.getElementById("sortBy").addEventListener("change", sortBy);
 
